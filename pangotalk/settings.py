144917b95ce23310cc9e3711eaff35d226bd14d1
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-21 16:40:26 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -180,10 +180,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-
-)
 
 
 # CSRF and Session settings
