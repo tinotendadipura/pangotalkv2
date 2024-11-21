851fdc9600d66d86c83a0f8d6f48c39dafc4049e
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-21 16:39:30 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -176,7 +176,9 @@ USE_TZ = False
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
