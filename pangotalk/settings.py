4e588139b4be9ae5b5714f675221fee88c0cb4b9
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-21 11:29:24 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from django.conf import DEFAULT_FILE_STORAGE_DEPRECATED_MSG
-
+#
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
