3539b2fb41e7a9de1c6b4ea3837dc19b30a360cb
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-24 02:34:28 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -29,8 +29,6 @@ SECRET_KEY = '!_@y-*=&qu=alggxs3d&hjbb58=f#)(ml+i^6evinx8q&a=+!j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_TRUSTED_ORIGINS = ['pangotalk.com']
 
 ALLOWED_HOSTS = ['pangotalk.com','.pangotalk.com']
     
@@ -247,4 +245,10 @@ AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ['pangotalk.com']
+
 
