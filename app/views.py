78d244a4ea6f18973c7819699cb6c2ac3f33fd4d
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-27 12:14:15 +0200

    push

--- app/views.py
+++ app/views.py
@@ -1987,3 +1987,14 @@ def message_balance(request):
     return render(request,'app/message_balance.html',context)
 
 
+def custom_404_view(request, exception):
+    return render(request, "404.html", {}, status=404)
+
+def custom_500_view(request):
+    return render(request, "500.html", {}, status=500)
+
+def custom_403_view(request, exception):
+    return render(request, "403.html", {}, status=403)
+
+def custom_400_view(request, exception):
+    return render(request, "400.html", {}, status=400)
