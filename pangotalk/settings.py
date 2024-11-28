8d486794e6c755862fc439860c7c72d6832bfef1
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-28 11:42:53 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!_@y-*=&qu=alggxs3d&hjbb58=f#)(ml+i^6evinx8q&a=+!j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['.pangotalk.com','pangotalk.com']
