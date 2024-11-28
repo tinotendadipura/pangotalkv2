0b3c7b74e0318a11e63a83af4fde8f9e03532e47
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-28 17:48:46 +0200

    push

--- pangotalk/urls.py
+++ pangotalk/urls.py
@@ -18,9 +18,9 @@ urlpatterns = [
     
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
 
 from django.conf.urls import handler500
 
