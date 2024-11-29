2e91e39c9f7a6b1c164e76c2b6d58e7dc11d8b40
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-11-29 12:06:37 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -197,24 +197,12 @@ USE_TZ = False
 
 
 STATIC_URL = '/static/'
+MEDIA__URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-
-# Google Cloud Storage
-GS_BUCKET_NAME = 'pangotalk-bucket'
-GS_PROJECT_ID   = 'pangotalk'
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-    os.path.join(BASE_DIR, 'credentials.json')
-)
-
-# Default file storage
-DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-
-# Media URL
-MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
-MEDIA_ROOT = "media/"
+MEDIA_ROOT  = '/home/tino/pangotalkv2/media'
     
-UPLOAD_ROOT = 'media/uploads/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
