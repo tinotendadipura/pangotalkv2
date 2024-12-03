a65b18603817ef6d8fe2a5c74e89fbd6b03adf04
Tinotenda Dipura <tinotendadipura2@gmail.com>
2024-12-03 12:41:01 +0200

    push

--- pangotalk/settings.py
+++ pangotalk/settings.py
@@ -250,4 +250,4 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Media files settings (optional)
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'sdou 
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
